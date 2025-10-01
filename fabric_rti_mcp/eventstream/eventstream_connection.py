9a4ebc4b5fd6cb9921c4fb7462fd1a43356cb44c
Niall Egan (from Dev Box) <niallegan@microsoft.com>
2025-10-01 17:57:45 +0100

    .

--- fabric_rti_mcp/eventstream/eventstream_connection.py
+++
@@ -1,9 +0,0 @@
-# Import and re-export for backward compatibility
-from fabric_rti_mcp.utils.fabric_api_http_client import (
-    FabricAPIHttpClient,
-    FabricHttpClientCache,
-    DEFAULT_FABRIC_HTTP_CLIENT_CACHE,
-)
-
-# Backward compatibility alias
-EventstreamConnection = FabricAPIHttpClient
